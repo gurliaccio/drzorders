943d19e9e67696606278d07b3de42044cb40b499
Michael Gurliaccio <gurliaccio@gmail.com>
2015-06-26 18:05:28 -0400

    refixed

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   resources :sessions,  only:   [:create]
 
   get '/index',     to: 'items#index'
-  get '/about',     to: 'static_pages#about'
   get '/signup',    to: 'users#new'
   get '/signin',    to: 'sessions#new'
   delete '/signout',   to: 'sessions#destroy'
